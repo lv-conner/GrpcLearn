960a37037768629090532f810b928b5c61f40f4a
lv-conner <lvmh123456@gmail.com>
2019-09-17 12:18:00 +0800

    修改

--- GrpcLearn.Client/Program.cs
+++ GrpcLearn.Client/Program.cs
@@ -12,17 +12,29 @@ namespace GrpcLearn.Client
     {
         static async Task Main(string[] args)
         {
-            var channel = GrpcChannel.ForAddress("https://localhost:8888");
-            //var channel = new Channel("localhost:8888", ChannelCredentials.Insecure);
+            //when host in Grpc.AspNetCore.Server use this;
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            //when use this,it can work;
+            //var channel = GrpcChannel.ForAddress("http://localhost:8888", new GrpcChannelOptions()
+            //{
+            //    Credentials = ChannelCredentials.Insecure
+            //});
+            //then host in aspnetcore with https; use this;
+            //var channel = GrpcChannel.ForAddress("https://localhost:8888");
+            //use this will work when service host in aspnetcore.but can not work when service host in Grpc.Core.Server
+            var channel = new Channel("localhost:8888", ChannelCredentials.Insecure);
             var client = new HelloMessageService.HelloService.HelloServiceClient(channel);
+            var helloRes = client.SayHello(new HelloMessage.HelloMessageRequest() { Name = "tim lv" });
+
             var res = await client.SayHelloAsync(new HelloMessage.HelloMessageRequest() { Name = "tim lv" });
             Console.WriteLine(res.Message);
 
             var call = client.SayHelloStreamVersion();
             var readTask = Task.Run(async () =>
             {
-                await foreach (var message in call.ResponseStream.ReadAllAsync())
+                while(await call.ResponseStream.MoveNext())
                 {
+                    var message = call.ResponseStream.Current;
                     Console.WriteLine(message);
                 }
             });
